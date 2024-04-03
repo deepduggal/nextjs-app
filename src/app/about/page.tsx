1cf6942f8b0196d92f29c286d576d6971a53da27
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2024-04-02 21:33:41 -0400

    Project setup. Assignment 1 done

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to our About page!</p>
+      <p>We are a team of developers passionate about creating amazing software.</p>
+    </div>
+  );
+}
